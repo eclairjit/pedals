9c0ebb41e1bfaf0d39b9a3b03190c0365ad015fa
Jit <ajblazejit95@gmail.com>
2024-05-06 16:43:48 +0530

    chore: Add environment variables and multer middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,10 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-export { app };
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser);
+
+export default app;
