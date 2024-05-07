9bbeaff198878433f1d4b0c831b6932fccb2947f
Jit <ajblazejit95@gmail.com>
2024-05-07 10:42:21 +0530

    chore: Update frontend dependencies and remove unused files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-import { useDispatch } from "react-redux";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const dispatch = useDispatch();
 
   return (
     <>
