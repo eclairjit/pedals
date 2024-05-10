7a8c6206d549f4d3f364c67dbaeaee76462829d1
Jit <ajblazejit95@gmail.com>
2024-05-11 05:13:38 +0530

    refactor: Update CycleCard component to include proper styling for the book button

--- frontend/src/pages/Cycles.jsx
+++ frontend/src/pages/Cycles.jsx
@@ -3,7 +3,43 @@ import CycleCard from "../components/CycleCard";
 
 const Cycles = () => {
   return (
-    <div className="w-screen flex flex-col items-center">
+    <div className="w-screen flex flex-wrap justify-center gap-4 items-center">
+      <CycleCard
+        model={"Hero Sprint"}
+        rentRate={"20 per hour"}
+        landmark={"Aquamarine"}
+        owner={"Jit Mitra"}
+      />
+      <CycleCard
+        model={"Hero Sprint"}
+        rentRate={"20 per hour"}
+        landmark={"Aquamarine"}
+        owner={"Jit Mitra"}
+      />
+      <CycleCard
+        model={"Hero Sprint"}
+        rentRate={"20 per hour"}
+        landmark={"Aquamarine"}
+        owner={"Jit Mitra"}
+      />
+      <CycleCard
+        model={"Hero Sprint"}
+        rentRate={"20 per hour"}
+        landmark={"Aquamarine"}
+        owner={"Jit Mitra"}
+      />
+      <CycleCard
+        model={"Hero Sprint"}
+        rentRate={"20 per hour"}
+        landmark={"Aquamarine"}
+        owner={"Jit Mitra"}
+      />
+      <CycleCard
+        model={"Hero Sprint"}
+        rentRate={"20 per hour"}
+        landmark={"Aquamarine"}
+        owner={"Jit Mitra"}
+      />
       <CycleCard
         model={"Hero Sprint"}
         rentRate={"20 per hour"}
